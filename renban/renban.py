aa3658ed3d1d0ffdf2015457c49d505c5d0b6829
Yuya Oka <nnsnodnb@gmail.com>
2021-08-02 19:59:25 +0900

    Fetch files from --input-path

--- renban/renban.py
+++ renban/renban.py
@@ -1,5 +1,6 @@
 import sys
-from typing import Optional
+from pathlib import Path
+from typing import Tuple, Optional
 
 import click
 
@@ -7,9 +8,11 @@ import click
 @click.command()
 @click.option("--prefix", "-p", type=str, default=None)
 @click.option("--start-point", "-s", type=int, default=1)
-def cmd(prefix: Optional[str], start_point: int) -> None:
-    click.echo(prefix)
-    click.echo(start_point)
+@click.option("--input-path", "-i", type=str, default="./")
+@click.option("--excludes", "-e", multiple=True)
+def cmd(prefix: Optional[str], start_point: int, input_path: str, excludes: Tuple[str]) -> None:
+    _input_path = Path(input_path)
+    files = [f for f in _input_path.iterdir() if f.is_file() and f.name not in excludes]
 
 
 if __name__ == "__main__":
