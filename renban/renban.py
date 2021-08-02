b214ceea159ac4918e43cecc5d8109fade52fbfe
Yuya Oka <nnsnodnb@gmail.com>
2021-08-02 09:59:18 +0900

    WIP

--- renban/renban.py
+++ renban/renban.py
@@ -1,9 +1,16 @@
 import sys
+from typing import Optional
+
+import click
 
 
-def main() -> None:
-    pass
+@click.command()
+@click.option("--prefix", "-p", type=str, default=None)
+@click.option("--start-point", "-s", type=int, default=1)
+def cmd(prefix: Optional[str], start_point: int) -> None:
+    click.echo(prefix)
+    click.echo(start_point)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(cmd())
